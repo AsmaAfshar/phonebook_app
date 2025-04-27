f173fa513c98ea9a05a5a2326af722137c7c99db
Asma Afshar <161859558+AsmaAfshar@users.noreply.github.com>
2025-04-27 16:14:37 +0500

    phonebook_app.py

--- phonebook_app.py
+++ phonebook_app.py
@@ -20,7 +20,7 @@ st.set_page_config(page_title="📱 Phonebook", layout="centered")
 st.title("📱 Stylish Phonebook App")
 
 # Load CSS
-with open("assets/style.css") as f:
+with open("style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 # Menu
